435e9946668c3d7a5def6f7567b1cd2abe8264b7
Jan Elznic <jan@elznic.com>
2019-08-08 09:13:51 +0200

    PieChart component refactoring

--- src/Components/PieChartComponent.tsx
+++ src/Components/PieChartComponent.tsx
@@ -15,52 +15,50 @@ export const PieChartComponent = (props: IProps) => {
 
   useEffect(() => {
     if (props.data && d3Container.current) {
-      const svg = d3.select(d3Container.current);
+      const svg: any = d3.select(d3Container.current);
 
-      const radius = Math.min(props.width, props.height) / 2;
-      const color = d3.scaleOrdinal().range(props.colors);
+      const radius: number = Math.min(props.width, props.height) / 2;
+      const color: any = d3.scaleOrdinal().range(props.colors);
 
-      const vis = svg
-        .append('svg') // create the SVG element inside the <body>
-        .data([props.data]) // associate our data with the document
-        .attr('width', props.width) // set the width and height of our visualization (these will be attributes of the <svg> tag
+      const vis: any = svg
+        .append('svg')
+        .data([props.data])
+        .attr('width', props.width)
         .attr('height', props.height)
-        .append('g') // make a group to hold our pie chart
-        .attr('transform', `translate(${radius}, ${radius})`); // move the center of the pie chart from 0, 0 to radius, radius
+        .append('g')
+        .attr('transform', `translate(${radius}, ${radius})`);
 
-      const arc = d3
-        .arc() // this will create <path> elements for us using arc data
+      const arc: any = d3
+        .arc()
         .innerRadius(0)
         .outerRadius(radius);
 
-      const pie = d3
-        .pie() // this will create arc data for us given a list of values
-        .value((res: any) => res.value); // we must tell it out to access the value of each element in our data array
+      const pie: any = d3
+        .pie()
+        .value((res: any) => res.value);
 
-      const arcs = vis
-        .selectAll('g.slice') // this selects all <g> elements with class slice (there aren't any yet)
+      const arcs: any = vis
+        .selectAll('g.slice')
         // @ts-ignore
-        .data(pie) // associate the generated pie data (an array of arcs, each having startAngle, endAngle and value properties)
-        .enter() // this will create <g> elements for every "extra" data element that should be associated with a selection. The result is creating a <g> for every object in the data array
-        .append('g') // create a group to hold each slice (we will have a <path> and a <text> element associated with each slice)
-        .attr('class', 'slice'); // allow us to style things in the slices (like text)
+        .data(pie)
+        .enter()
+        .append('g')
+        .attr('class', 'slice');
 
       arcs
         .append('path')
-        .attr('fill', (res: Expenses, i: any) => color(i)) // set the color for each slice to be chosen from the color function defined above
-        .attr('d', arc); // this creates the actual SVG path using the associated data (pie) with the arc drawing function
+        .attr('fill', (res: Expenses, i: any) => color(i))
+        .attr('d', arc);
 
       arcs
-        .append('text') // add a label to each slice
+        .append('text')
         .attr('transform', (res: any) => {
-          // set the label's origin to the center of the arc
-          // we have to make sure to set these before calling arc.centroid
           res.innerRadius = 0;
           res.outerRadius = radius;
-          return `translate(${arc.centroid(res)})`; // this gives us a pair of coordinates like [50, 50]
+          return `translate(${arc.centroid(res)})`;
         })
-        .attr('text-anchor', 'middle') //center the text on it's origin
-        .text((res: Expenses) => res.value); // get the label from our original data array
+        .attr('text-anchor', 'middle')
+        .text((res: Expenses) => res.value);
     }
   }, [props.data, props.width, props.height, props.colors]);
 
